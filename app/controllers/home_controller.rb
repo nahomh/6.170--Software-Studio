2aef9c0c7cc4b71c8e6a1a73a628287512b22c29
Nahom Workie <nahomh@ubuntu.(none)>
2013-04-25 21:03:14 -0400

    fixing the marker infowindow

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,13 @@
 class HomeController < ApplicationController
   def index
   	@rooms = Room.all
-    @json = @rooms.to_gmaps4rails
+    @json = @rooms.to_gmaps4rails do |room, marker|
+    	marker.infowindow render_to_string(:partial => "/home/infowindow", :locals => { :room => room})
+	    marker.title "#{room.name}"
+	    marker.json({ :description => room.description})
+	    marker.picture({:picture => "http://mapicons.nicolasmollet.com/     wp-content/uploads/mapicons/shape-default/color-3875d7/shapeco     lor-color/shadow-1/border-dark/symbolstyle-contrast/symbolshad     owstyle-dark/gradient-iphone/information.png",
+	                    :width => 32,
+	                    :height => 32})
+  	end
   end
 end
