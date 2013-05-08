8eadc252e322c708387395c558589c9e07c8090c
Tony Chen <tchen91@mit.edu>
2013-05-08 13:28:58 -0400

    radius lessened

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -28,7 +28,7 @@ class RoomsController < ApplicationController
         format.html { redirect_to @room, :notice => "Successfully checked room back in" }
         format.js {}
       else
-        if current_user.close_to_room(@room) < 1.0
+        if current_user.close_to_room(@room) <.5
           old_room = current_user.room
           current_user.checkout(@room.id)
           if old_room && old_room.users.size == 0
