99ba6c553b39422c4a10c000ef50e917a3240959
Nahom Workie <nahomh@ubuntu.(none)>
2013-05-12 11:34:48 -0400

    ad co

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,9 @@ class User < ActiveRecord::Base
   include Math
   attr_accessible :name, :oauth_expires_at, :oauth_token, :provider, :uid, :room_id, :longitude, :latitude
   belongs_to :room
+
+  #Methods
+  #Authentication via Omniauth
   def self.from_omniauth(auth)
     where(auth.slice(:provider, :uid)).first_or_initialize.tap do |user|
       puts auth
@@ -14,30 +17,43 @@ class User < ActiveRecord::Base
       user.save!
     end
   end
+  
+ #Requires:longitude, latitude
+ #Updates the current user's location
   def update_location(longitude, latitude)
     self.longitude = longitude
     self.latitude = latitude
     self.save
   end
+
+  #Requires: user, room_id
+  #Updates checkin for a user into a particular room.
   def checkin
     update_attributes(:room_id => nil)
   end
 
+  #Requires: user, room_id
+  #Updates:  Checks out a specific user from a room. 
   def checkout(room_id)
     update_attributes(:room_id => room_id)
   end
-
+  
+  #Requires: Graph
+  #Gets the user's facebook friends.
   def get_fb_friends(graph)
     friends = graph.get_connections("me", "friends")
     return friends
   end
 
+  #Requires:friend_id, graph
+  #Checks the user's facebook friends for a friends on MITSpaces.
   def facebook_friends? (friend_id, graph)
     fb_friends = get_fb_friends(graph)
     friend_fbids = fb_friends.map{|friend| friend["id"]}
     return friend_fbids.include?(User.find(friend_id).uid)
   end
-
+  #Requires: graph
+  #Gets friend_ids for the user's facebook friends
   def friend_ids(graph)
     fb_friends = get_fb_friends(graph)
     friend_fbids = fb_friends.map{|friend| friend["id"]}
@@ -45,13 +61,17 @@ class User < ActiveRecord::Base
     friend_ids = friends.map{|friend| friend.id}
     return friend_ids
   end
-
+ 
+  #Requires: graph
+  #Gets the uses where their ids are in the friends graph ids.
   def friends(graph)
     friends = User.where(:id => friend_ids(graph))
   end
-  #haversine formula to calculate distance between two coordinates
+  #Haversine formula to calculate distance between two coordinates
   #if the user does not have a location (in the case of browswer issues), 
   #we give the user the benefit of the doubt and allows them to checkin anywhere
+  #Requires: room
+  #Checks if the user's coordinates are close enough to a room's location
   def close_to_room(room)
     if self.latitude == nil && self.longitude == nil
       d = 0
